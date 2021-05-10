f0146c0896cc6e1b56de4c43dbc76a0d34204ce1
bgreene <bgreene777@gmail.com>
2021-05-10 22:23:13 +0000

    progress on automating post-processing tasks; still need to work on main()

--- python/run_calc_stats.py
+++ python/run_calc_stats.py
@@ -8,38 +8,111 @@
 # --------------------------------
 import numpy as np
 import os
-from time import sleep
 from datetime import datetime
+from argparse import ArgumentParser
 
-fdir = "/home/bgreene/simulations/F_128_interp/"
-fslurm = f"{fdir}slurm-2100.out"
-finished = False
+parser = ArgumentParser()
+parser.add_argument()
 
-while not finished:
-    # print timestamp
-    print(f"Attempting at {datetime.utcnow()} UTC")
-    # read slurm file
-    with open(fslurm) as f:
+# --------------------------------
+# Define functions for processing
+# --------------------------------
+def check_slurm_finished(fdir, fslurm):
+    # Read fdir+fslurm to check for completion
+    # Returns boolean
+    print(f"Reading {fdir}{fslurm} to determine if finished...")
+    with open(f"{fdir}{fslurm}") as f:
         txt = f.read()
     # check for completion
     if "END" in txt:
-        print("Simulation finished! Continue to run calc_stats")
-        finished=True
-        break
+        print(f"Simulation finished! Time: {datetime.utcnow()}")
+        return True
     else:
-        print("Simulation still running; wait 1 hour")
-        sleep(3600)
+        print(f"Simulation still running, try later. Time: {datetime.utcnow()}")
+        return False
+# --------------------------------    
+def check_already_processed(fdir):
+    # check for presence of calc_stats.txt in fdir, signifying 
+    # that calc_stats() has already been run
+    # Returns boolean
+    if os.path.exists(f"{fdir}calc_stats.txt"):
+        return True
+    elif os.path.exists(f"{fdir}interp3d.txt"):
+        return True
+    else:
+        return False
+# --------------------------------
+def calc_stats(fdir,exe):
+    # run the pre-compiled fortran executable
+    os.system(f"/home/bgreene/SBL_LES/fortran/executables/{exe}")
+    
+    # spit out a calc_stats.txt file so this isn't repeated
+    np.savetxt(f"{fdir}calc_stats.txt", fmt="%s",
+               [f"Calc stats complete! Time: {datetime.utcnow()}"])
+    
+    return
+# --------------------------------
+def run_interp3d(fdir,exe,dimsize):
+    # create new interp directory based on fdir (which points to spinup directory)
+    simdir, spinup, blah = fdir.rsplit("/", 2)
+    fnew = os.path.join(simdir, f"{spinup[0]}_{dimsize}_interp") + os.sep
+    print(f"Creating new directory: {fnew}")
+   
+    # rsync spinup directory into new one
+    os.system(f"rsync -avr {fdir} {fnew} --exclude-from={fdir}output/*")
+    
+    # move everything up one level 
+    os.system(f"mv {fnew}{spinup[0]}_spinup/* {fnew}")
+    
+    # create the new fields_3d directory
+    os.system(f"mkdir {fnew}output/fields_3d/")
+    
+    # rename vel_sc.out
+    os.system(f"mv {fnew}vel_sc.out {fnew}vel_sc_spinup.out")
 
-# # rsync spinup directory into new one
-# os.system("rsync -avr /home/bgreene/simulations/F_spinup /home/bgreene/simulations/F_128_interp/ --exclude-from=/home/bgreene/simulations/F_spinup/output/*")
-# # move everything up one level 
-# os.system("mv /home/bgreene/simulations/F_128_interp/F_spinup/* /home/bgreene/simulations/F_128_interp/")
-# # create the new fields_3d directory
-# os.system("mkdir /home/bgreene/simulations/F_128_interp/output/fields_3d/")
-# # rename vel_sc.out
-# os.system("mv /home/bgreene/simulations/F_128_interp/vel_sc.out /home/bgreene/simulations/F_128_interp/vel_sc_spinup.out")
-# # run the interp3d script
-# os.system("/home/bgreene/fortran/interp3d")
+    # run the interp3d script
+    os.system(f"/home/bgreene/SBL_LES/fortran/executables/{exe}")
+    
+    # spit out interp3d.txt file so this isn't repreated
+    np.savetxt(f"{fdir}interp3d.txt", fmt="%s",
+           [f"interp3d complete! Time: {datetime.utcnow()}"])
+    
+    print(f"Finished run_interp3d! Time: {datetime.utcnow()}")
+    
+    return
+# --------------------------------
+def main(dsim):
+    # before anything else, check if sim finished
+    sim_finished = check_slurm_finished(dsim[0], dsim[1])
+    # also check to see if calc_stats or interp3d have been run already
+    task_complete = check_already_processed(dsim[0])
+    if not sim_finished:
+        # not done -- print time and quit
+        print("run_calc_stats.py finished.")
+        return
+    elif (sim_finished & task_complete):
+        # already been performed -- print time and quit
+        print("Simulation finished and task already complete!")
+        print("run_calc_stats.py finished.")
+        return
+    elif (sim_finished & (not task_complete)):
+        # task not complete -- decide which to do
+        
+        return
+    
+    else:
+        print("idk how you got here")
+        
 
-os.system("/home/bgreene/fortran/analysis/calc_stats")
+
+# --------------------------------
+# Load simulations.txt to determine tasks
+# --------------------------------
+
+fsim = "/home/bgreene/SBL_LES/python/simulations.txt"
+dsim = np.genfromtxt(fsim, delimiter=",", dtype=str, skip_headers=1)
+
+
+
+
 print(f"Finished! Time: {datetime.utcnow()} UTC")
