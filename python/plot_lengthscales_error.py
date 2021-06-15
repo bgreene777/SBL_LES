1a9181d057d1fdb6b7596bd85544e003eedaa186
bgreene <bgreene777@gmail.com>
2021-06-15 20:25:36 +0000

    update to (hopefully) also plot variances uu vv ww thetatheta

--- python/plot_lengthscales_error.py
+++ python/plot_lengthscales_error.py
@@ -156,21 +156,89 @@ def plot_int_len(slist, fsave=fdir_save):
     fig2.savefig(fsave2, format="pdf", bbox_inches="tight")
     plt.close(fig2)
     
+#
+    # 3) u'u', v'v', w'w', theta'theta'
+    #
+    # create figure and axes handles
+    fig3, ax3 = plt.subplots(nrows=2,ncols=4, sharey=True, figsize=(16, 12))
+    # row 1: length scales
+    # u'u' length scale
+    ax3[0,0].plot(s.RFM["len_uu"], s.z[s.RFM["isbl"]]/s.h, color=colors[1], linestyle="-")
+    # v'v' length scale
+    ax3[0,1].plot(s.RFM["len_vv"], s.z[s.RFM["isbl"]]/s.h, color=colors[1], linestyle="-")
+    # w'w' length scale
+    ax3[0,2].plot(s.RFM["len_ww"], s.z[s.RFM["isbl"]]/s.h, color=colors[1], linestyle="-")
+    # w'w' length scale
+    ax3[0,3].plot(s.RFM["len_tt"], s.z[s.RFM["isbl"]]/s.h, color=colors[1], linestyle="-")
+    # row 2: time scales
+    # u'u' length scale
+    ax3[1,0].plot(s.RFM["len_uu"]/s.xytavg["ws"][s.RFM["isbl"]], s.z[s.RFM["isbl"]]/s.h, 
+                  color=colors[1], linestyle="-")
+    # v'v' length scale
+    ax3[1,1].plot(s.RFM["len_vv"]/s.xytavg["ws"][s.RFM["isbl"]], s.z[s.RFM["isbl"]]/s.h, 
+                  color=colors[1], linestyle="-")
+    # w'w' length scale
+    ax3[1,2].plot(s.RFM["len_ww"]/s.xytavg["ws"][s.RFM["isbl"]], s.z[s.RFM["isbl"]]/s.h, 
+                  color=colors[1], linestyle="-") 
+    # theta'theta' length scale
+    ax3[1,3].plot(s.RFM["len_tt"]/s.xytavg["ws"][s.RFM["isbl"]], s.z[s.RFM["isbl"]]/s.h, 
+                  color=colors[1], linestyle="-") 
+    # also plot zj as horiz line
+    for iax in ax3.ravel():
+        iax.axhline(s.xytavg["zj"]/s.h, color=colors[1], linestyle="--", label="$z_{LLJ}$")
+
+    # format figures
+    # u'u' length scale
+    ax3[0,0].set_xlabel("$\mathcal{L}_{u'u'}$ [m]")
+    ax3[0,0].set_ylabel("$z/h$")
+    ax3[0,0].grid()
+#     ax1[0,0].legend()
+    ax3[0,0].set_ylim([-0.05, 1.2])
+    # v'v' length scale
+    ax3[0,1].set_xlabel("$\mathcal{L}_{v'v'}$ [m]")
+    ax3[0,1].grid()
+    # w'w' length scale
+    ax3[0,2].set_xlabel("$\mathcal{L}_{w'w'}$ [m]")
+    ax3[0,2].grid()
+    # theta'theta' length scale
+    ax3[0,3].set_xlabel("$\mathcal{L}_{\\theta'\\theta'}$ [m]")
+    ax3[0,3].grid()
+    # u'u' time scale
+    ax3[1,0].set_xlabel("$\mathcal{T}_{u'u'}$ [m] [s]")
+    ax3[1,0].set_ylabel("$z/h$")
+    ax3[1,0].grid()
+#     ax1[1,0].legend()
+    # v'v' time scale
+    ax3[1,1].set_xlabel("$\mathcal{T}_{v'v'}$ [m] [s]")
+    ax3[1,1].grid()
+    # w'w' time scale
+    ax3[1,2].set_xlabel("$\mathcal{T}_{w'w'}$ [s]")
+    ax3[1,2].grid()
+    # theta'theta' time scale
+    ax3[1,3].set_xlabel("$\mathcal{T}_{\\theta'\\theta'}$ [s]")
+    ax3[1,3].grid()
+
+    # save figure
+    fsave3 = f"{fsave}{s.stab}_var_lengthscales.pdf"
+    print(f"Saving figure: {fsave3}")
+    fig3.savefig(fsave3, format="pdf", bbox_inches="tight")
+    plt.close(fig3)
+    
     return
 # --------------------------------
 
 def plot_sigma_filt(s, fsave=fdir_save):
     """
-    6 total figures to loop over:
-    u, v, theta, u'w', v'w', theta'w'
+    10 total figures to loop over:
+    u, v, theta, u'w', v'w', theta'w', u'u', v'v', w'w', theta'theta'
     """
     # first determine plotting parameters
-    kplot = []
-    for key in s.RFM.keys():
-        if key[:4] == "var_":
-            kplot.append(key)
+    kplot = ["var_u", "var_v", "var_theta", "var_uw", "var_vw", "var_tw",
+             "var_uu", "var_vv", "var_ww", "var_tt"]
     splot = ["$\sigma_u$", "$\sigma_v$", "$\sigma_{\\theta}$", 
-             "$\sigma_{u'w'}$", "$\sigma_{v'w'}$", "$\sigma_{\\theta'w'}$"]
+             "$\sigma_{u'w'}$", "$\sigma_{v'w'}$", "$\sigma_{\\theta'w'}$",
+             "$\sigma_{u'u'}$", "$\sigma_{v'v'}$", "$\sigma_{w'w'}$",
+             "$\sigma_{\\theta'\\theta'}$"]
     # begin looping over parameters
     for ivar, var in enumerate(kplot):
         svar = var.split("_")[-1]
@@ -217,11 +285,12 @@ def plot_sigma_filt(s, fsave=fdir_save):
 
 def plot_err(s, fsave=fdir_save):
     """
-    6 total figures to loop over:
-    u, v, theta, u'w', v'w', theta'w'
+    10 total figures to loop over:
+    u, v, theta, u'w', v'w', theta'w', u'u', v'v', w'w', theta'theta'
     """
-    vplot = ["u", "v", "\\theta", "u'w'", "v'w'", "\\theta'w'"]
-    for ivar, var in enumerate(["u", "v", "theta", "uw", "vw", "tw"]):
+    vplot = ["u", "v", "\\theta", "u'w'", "v'w'", "\\theta'w'", 
+             "u'u'", "v'v'", "w'w'", "\\theta'\\theta'"]
+    for ivar, var in enumerate(["u", "v", "theta", "uw", "vw", "tw", "uu", "vv", "ww", "tt"]):
         # initialize figure
         fig1, ax1 = plt.subplots(1, figsize=(12, 8))
         # plot RFM errors
@@ -239,7 +308,10 @@ def plot_err(s, fsave=fdir_save):
             ax1.set_xscale("log")
         ax1.set_xlabel(f"$\epsilon_{{{vplot[ivar]}}}$ [$\%$]")
         ax1.set_ylabel("$z$ [m]")
-        T_sample = s.RFM["yaml"][()]["T_sample"]
+        if var in ["u", "v", "theta"]:
+            T_sample = s.RFM["yaml"][()]["T_sample_u"]
+        else:
+            T_sample = s.RFM["yaml"][()]["T_sample_cov"]
         ax1.set_title(f"Relative Random Error in ${{{vplot[ivar]}}}$, {s.stab}{s.lab}, $T={{{T_sample}}}$ s")
         # save figure
         fsave1 = f"{fsave}{s.stab}{s.lab}_{var}_err.pdf"
@@ -252,18 +324,24 @@ def plot_err(s, fsave=fdir_save):
 
 def plot_MSE(s, fsave=fdir_save):
     """
-    6 total figures to loop over:
-    u, v, theta, u'w', v'w', theta'w'
+    10 total figures to loop over:
+    u, v, theta, u'w', v'w', theta'w', u'u', v'v', w'w', theta'theta'
     """
-    vplot = ["u", "v", "\\theta", "u'w'", "v'w'", "\\theta'w'"]
+    vplot = ["u", "v", "\\theta", "u'w'", "v'w'", "\\theta'w'", 
+             "u'u'", "v'v'", "w'w'", "\\theta'\\theta'"]
     variance = {"u": s.var["u_var_tot"],
                 "v": s.var["v_var_tot"],
                 "t": s.var["theta_var_tot"],
                 "uw": s.RFM["uwuw_var_xytavg"],
                 "vw": s.RFM["vwvw_var_xytavg"],
-                "tw": s.RFM["twtw_var_xytavg"]}
+                "tw": s.RFM["twtw_var_xytavg"],
+                "uu": s.RFM["uuuu_var_xytavg"],
+                "vv": s.RFM["vvvv_var_xytavg"],
+                "ww": s.RFM["wwww_var_xytavg"],
+                "tt": s.RFM["tttt_var_xytavg"]
+               }
     
-    for ivar, var in enumerate(["u", "v", "t", "uw", "vw", "tw"]):
+    for ivar, var in enumerate(["u", "v", "t", "uw", "vw", "tw", "uu", "vv", "ww", "tt"]):
         # annoying bit of code to keep t/theta consistent in var names
         if var == "t":
             var2 = "theta"
@@ -331,7 +409,7 @@ s192F = simulation("/home/bgreene/simulations/F_192_interp/output/",
 # put everything into a list for looping
 # s_all = [s128A, s160A, s192A]
 # s_all = [s128F, s160F, s192F]
-s_all = [s192A]
+s_all = [s192F]
 for s in s_all:
     s.read_csv()
     s.calc_Ri()
