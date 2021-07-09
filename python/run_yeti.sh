af8a9b2221969e958f11d09533f5dbfbc5538e59
bgreene <bgreene777@gmail.com>
2021-07-09 21:46:35 +0000

    batch file to run RFM.py on a compute node on Yeti

---
+++ python/run_yeti.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name=RFM.py
+#SBATCH --partition=CLUSTER
+#SBATCH -N 1
+#SBATCH -c 56
+#SBATCH -o out.out
+#SBATCH -e err.log
+#SBATCH --exclusive
+#SBATCH --time=504:00:00
+#SBATCH --mail-user=brian.greene@ou.edu
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+
+#Set stack size, etc.
+ulimit -s unlimited
+ulimit -c unlimited
+
+echo ' ****** START OF JOB ****** '
+date
+srun  /home/bgreene/anaconda3/bin/python -u /home/bgreene/SBL_LES/python/RFM.py
+date
+echo ' ****** END OF JOB ****** '
