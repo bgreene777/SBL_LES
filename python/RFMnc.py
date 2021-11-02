1d2d80e0e2976ee7f4a744fabb406c8e2681d553
bgreene <bgreene777@gmail.com>
2021-11-02 19:58:06 +0000

    use if __name__... syntax; save netcdf files for 4th order variances, RFM output, and autocorrelation calculations for use later

--- python/RFMnc.py
+++ python/RFMnc.py
@@ -17,6 +17,7 @@ from numpy.fft import fft, ifft
 from scipy.signal import fftconvolve, correlate
 from datetime import datetime, timedelta
 from numba import njit
+from dask.diagnostics import ProgressBar
 # --------------------------------
 # Define Functions
 # --------------------------------
@@ -25,6 +26,8 @@ def autocorrelation(f):
     # output DataArray yt-averaged autocorrelation R_ff(xlag,z)
     # R_ff.shape = (nx, nz)
     # calculate along the x-dimension
+    # keep track of how long it takes
+    dt0 = datetime.utcnow()
     print(f"Calculating autocorrelation for: {f.name}")
     # calculate normalized perturbations
     temp = (f - f.mean("x")) / f.std("x")
@@ -41,6 +44,8 @@ def autocorrelation(f):
                             x = f.x,
                             z = f.z)
                        )
+    dt1 = datetime.utcnow()
+    print(f"Calc time for {f.name} autocorr: {(dt1-dt0).total_seconds()/60.:5.2f} min")
     return R_ff
 # --------------------------------
 def relaxed_filter(f, delta_x, Lx):
@@ -52,6 +57,9 @@ def relaxed_filter(f, delta_x, Lx):
     -output-
     var_f_all: xarray DataArray of yt-averaged var along x as function of delta_x and z
     """
+    # keep track of time
+    dt0 = datetime.utcnow()
+    print(f"Performing relaxed filter method for: {f.name}")
     # initialize numpy array of shape(nfilt, nz) to store all sigma_f
     nfilt = len(delta_x)
     nx = f.x.size
@@ -102,121 +110,187 @@ def relaxed_filter(f, delta_x, Lx):
     # divide by nt to get time-averaged sigma_f_all
     var_f_all /= nt
     
-    return sigma_f_all
+    dt1 = datetime.utcnow()
+    print(f"Calc time for {f.name} RFM: {(dt1-dt0).total_seconds()/60.:5.2f} min")
+    return var_f_all
     
 # --------------------------------
 # Main
 # --------------------------------
-# load yaml file
-with open("/home/bgreene/SBL_LES/python/RFMnc.yaml") as f:
-    config = yaml.safe_load(f)
+def main():
+    # load yaml file
+    with open("/home/bgreene/SBL_LES/python/RFMnc.yaml") as f:
+        config = yaml.safe_load(f)
+
+    # simulation output directory
+    fdir = config["fdir"]
+    # load average statistics netcdf file
+    fstat = config["fstat"]
+    stat = xr.load_dataset(fdir+fstat)
+    # calculate important parameters
+    # ustar
+    stat["ustar"] = ((stat.uw_cov_tot ** 2.) + (stat.vw_cov_tot ** 2.)) ** 0.25
+    stat["ustar2"] = stat.ustar ** 2.
+    # SBL height
+    stat["h"] = stat.z.where(stat.ustar2 <= 0.05*stat.ustar2[0], drop=True)[0]/0.95
+    # z indices within sbl
+    isbl = np.where(stat.z <= stat.h)[0]
+    nz_sbl = len(isbl)
+
+    #
+    # Load files and clean up
+    #
+    # prepare to load all individual netcdf timestep files
+    timesteps = np.arange(config["t0"], config["t1"]+1, config["dt"], dtype=np.int32)
+    # determine files to read from timesteps
+    fall = [f"{fdir}all_{tt:07d}.nc" for tt in timesteps]
+    nf = len(fall)
+    # calculate array of times represented by each file
+    times = np.array([i*config["delta_t"]*config["dt"] for i in range(nf)])
+    # load all files into mfdataset
+    print("Reading files...")
+    dd = xr.open_mfdataset(fall, combine="nested", concat_dim="time")
+    dd.coords["time"] = times
+    dd.time.attrs["units"] = "s"
+    # array of delta_x spaced logarithmic from dx to Lx
+    nfilt = config["nfilt"]
+    delta_x = np.logspace(np.log10(dd.dx), np.log10(dd.Lx),
+                          num=nfilt, base=10.0, dtype=np.float64)
+
+    #
+    # Rotate instantaneous (u,v) using u_mean and v_mean from stat
+    #
+    angle = np.arctan2(stat.v_mean, stat.u_mean)
+    dd["u_rot"] = dd.u*np.cos(angle) + dd.v*np.sin(angle)  # xarray takes care of dimensions
+    dd["v_rot"] =-dd.u*np.sin(angle) + dd.v*np.cos(angle)
+
+    #
+    # Calculate "instantaneous" vars and covars to store in dd
+    #
+    print("Calculate 'instantaneous' vars and covars")
+    # u'w'
+    dd["uw_cov_res"] = (dd.u - stat.u_mean) * (dd.w - stat.w_mean)
+    dd["uw_cov_tot"] = dd.uw_cov_res + dd.txz
+    # v'w'
+    dd["vw_cov_res"] = (dd.v - stat.v_mean) * (dd.w - stat.w_mean)
+    dd["vw_cov_tot"] = dd.vw_cov_res + dd.tyz
+    # t'w'
+    dd["tw_cov_res"] = (dd.theta - stat.theta_mean) * (dd.w - stat.w_mean)
+    dd["tw_cov_tot"] = dd.tw_cov_res + dd.q3
+    # u'u' UNROTATED
+    dd["uu_var"] = (dd.u - stat.u_mean) * (dd.u - stat.u_mean)
+    # u'u' ROTATED
+    dd["uu_var_rot"] = (dd.u_rot - stat.u_mean_rot) * (dd.u_rot - stat.u_mean_rot)
+    # v'v' UNROTATED
+    dd["vv_var"] = (dd.v - stat.v_mean) * (dd.v - stat.v_mean)
+    # v'v' ROTATED
+    dd["vv_var_rot"] = (dd.v_rot - stat.v_mean_rot) * (dd.v_rot - stat.v_mean_rot)
+    # w'w'
+    dd["ww_var"] = (dd.w - stat.w_mean) * (dd.w - stat.w_mean)
+    # t't'
+    dd["tt_var"] = (dd.theta - stat.theta_mean) * (dd.theta - stat.theta_mean)
+
+    #
+    # Use "instantaneous" vars and covars to calculate 4th order variances
+    #
+    print("Calculate 4th order vars")
+    # u'w'u'w' = var{u'w'}(x,y,z,t) = (u'w' - <u'w'>_xyzt)**2
+    dd["uwuw_var"] = (dd.uw_cov_tot - stat.uw_cov_tot) * (dd.uw_cov_tot - stat.uw_cov_tot)
+    # v'w'v'w' = var{v'w'}(x,y,z,t) = (v'w' - <v'w'>_xyzt)**2
+    dd["vwvw_var"] = (dd.vw_cov_tot - stat.vw_cov_tot) * (dd.vw_cov_tot - stat.vw_cov_tot)
+    # t'w't'w' = var{t'w'}(x,y,z,t) = (t'w' - <t'w'>_xyzt)**2
+    dd["twtw_var"] = (dd.tw_cov_tot - stat.tw_cov_tot) * (dd.tw_cov_tot - stat.tw_cov_tot)
+    # UNROTATED u'u'u'u' = var{u'u'}(x,y,z,t) = (u'u' - <u'u'>_xyzt)**2
+    dd["uuuu_var"] = (dd.uu_var - stat.u_var) * (dd.uu_var - stat.u_var)
+    # ROTATED u'u'u'u' = var{u'u'}(x,y,z,t) = (u'u' - <u'u'>_xyzt)**2
+    dd["uuuu_var_rot"] = (dd.uu_var_rot - stat.u_var_rot) * (dd.uu_var_rot - stat.u_var_rot)
+    # UNROTATED v'v'v'v' = var{v'v'}(x,y,z,t) = (v'v' - <v'v'>_xyzt)**2
+    dd["vvvv_var"] = (dd.vv_var - stat.v_var) * (dd.vv_var - stat.v_var)
+    # ROTATED v'v'v'v' = var{v'v'}(x,y,z,t) = (v'v' - <v'v'>_xyzt)**2
+    dd["vvvv_var_rot"] = (dd.vv_var_rot - stat.v_var_rot) * (dd.vv_var_rot - stat.v_var_rot)
+    # w'w'w'w' = var{w'w'}(x,y,z,t) = (w'w' - <w'w'>_xyzt)**2
+    dd["wwww_var"] = (dd.ww_var - stat.w_var) * (dd.ww_var - stat.w_var)
+    # t't't't' = var{t't'}(x,y,z,t) = (t't' - <t't'>_xyzt)**2
+    dd["tttt_var"] = (dd.tt_var - stat.theta_var) * (dd.tt_var - stat.theta_var)
     
-# simulation output directory
-fdir = config["fdir"]
-# load average statistics netcdf file
-fstat = config["fstat"]
-stat = xr.load_dataset(fdir+fstat)
-# calculate important parameters
-# ustar
-stat["ustar"] = ((stat.uw_cov_tot ** 2.) + (stat.vw_cov_tot ** 2.)) ** 0.25
-stat["ustar2"] = stat.ustar ** 2.
-# SBL height
-stat["h"] = stat.z.where(stat.ustar2 <= 0.05*stat.ustar2[0], drop=True)[0]/0.95
-# z indices within sbl
-isbl = np.where(stat.z <= stat.h)[0]
-nz_sbl = len(isbl)
+    #
+    # Calculate xyt averages of 4th order variances and store in new xarray Dataset to save
+    #
+    var4 = xr.Dataset(data_vars=None,
+                      coords=dict(z=dd.z),
+                      attrs=stat.attrs
+                     )
+    # define list of parameters for looping
+    param_var4 = ["uwuw_var", "vwvw_var", "twtw_var", "uuuu_var", "uuuu_var_rot",
+                  "vvvv_var", "vvvv_var_rot", "wwww_var", "tttt_var"]
+    # loop
+    for p in param_var4:
+        var4[p] = dd[p].mean(dim=("x","y","time"))
 
-#
-# Load files and clean up
-#
-# prepare to load all individual netcdf timestep files
-timesteps = np.arange(config["t0"], config["t1"]+1, config["dt"], dtype=np.int32)
-# determine files to read from timesteps
-fall = [f"{fdir}all_{tt:07d}.nc" for tt in timesteps]
-nf = len(fall)
-# calculate array of times represented by each file
-times = np.array([i*config["delta_t"]*config["dt"] for i in range(nf)])
-# load all files into mfdataset
-print("Reading files...")
-dd = xr.open_mfdataset(fall, combine="nested", concat_dim="time")
-dd.coords["time"] = times
-dd.time.attrs["units"] = "s"
-# array of delta_x spaced logarithmic from dx to Lx
-nfilt = config["nfilt"]
-delta_x = np.logspace(np.log10(dd.dx), np.log10(dd.Lx),
-                      num=nfilt, base=10.0, dtype=np.float64)
+    #
+    # Calculate autocorrelations for _everything_
+    #
+    print("Begin calculating autocorrelations...")
+    # initialize empty xarray dataset for each autocorrelation and loop
+    R = xr.Dataset(data_vars=None,
+                   coords=dict(x=dd.x,
+                               z=dd.z),
+                   attrs=dd.attrs
+                  )
+    # define list of parameters for looping
+    param_acf = ["u", "u_rot", "v", "v_rot", "w", "theta",
+                 "uw_cov_tot", "vw_cov_tot", "tw_cov_tot", "uu_var", "uu_var_rot", 
+                 "vv_var", "vv_var_rot", "ww_var", "tt_var",
+                 "uwuw_var", "vwvw_var", "uuuu_var", "uuuu_var_rot", 
+                 "vvvv_var", "vvvv_var_rot", "wwww_var", "tttt_var"]
+    for p in param_acf:
+        R[p] = autocorrelation(dd[p])
 
-#
-# Rotate instantaneous (u,v) using u_mean and v_mean from stat
-#
-angle = np.arctan2(stat.v_mean, stat.u_mean)
-dd["u_rot"] = dd.u*np.cos(angle) + dd.v*np.sin(angle)  # xarray takes care of dimensions
-dd["v_rot"] =-dd.u*np.sin(angle) + dd.v*np.cos(angle)
-
-#
-# Calculate "instantaneous" vars and covars to store in dd
-#
-print("Calculate 'instantaneous' vars and covars")
-# u'w'
-dd["uw_cov_res"] = (dd.u - stat.u_mean) * (dd.w - stat.w_mean)
-dd["uw_cov_tot"] = dd.uw_cov_res + dd.txz
-# v'w'
-dd["vw_cov_res"] = (dd.v - stat.v_mean) * (dd.w - stat.w_mean)
-dd["vw_cov_tot"] = dd.vw_cov_res + dd.tyz
-# t'w'
-dd["tw_cov_res"] = (dd.theta - stat.theta_mean) * (dd.w - stat.w_mean)
-dd["tw_cov_tot"] = dd.tw_cov_res + dd.q3
-# u'u' UNROTATED
-dd["uu_var"] = (dd.u - stat.u_mean) * (dd.u - stat.u_mean)
-# u'u' ROTATED
-dd["uu_var_rot"] = (dd.u_rot - stat.u_mean_rot) * (dd.u_rot - stat.u_mean_rot)
-# v'v' UNROTATED
-dd["vv_var"] = (dd.v - stat.v_mean) * (dd.v - stat.v_mean)
-# v'v' ROTATED
-dd["vv_var_rot"] = (dd.v_rot - stat.v_mean_rot) * (dd.v_rot - stat.v_mean_rot)
-# w'w'
-dd["ww_var"] = (dd.w - stat.w_mean) * (dd.w - stat.w_mean)
-# t't'
-dd["tt_var"] = (dd.theta - stat.theta_mean) * (dd.theta - stat.theta_mean)
-
-#
-# Use "instantaneous" vars and covars to calculate 4th order variancess
-#
-print("Calculate 4th order vars")
-# u'w'u'w' = var{u'w'}(x,y,z,t) = (u'w' - <u'w'>_xyzt)**2
-dd["uwuw_var"] = (dd.uw_cov_tot - stat.uw_cov_tot) * (dd.uw_cov_tot - stat.uw_cov_tot)
-# v'w'v'w' = var{v'w'}(x,y,z,t) = (v'w' - <v'w'>_xyzt)**2
-dd["vwvw_var"] = (dd.vw_cov_tot - stat.vw_cov_tot) * (dd.vw_cov_tot - stat.vw_cov_tot)
-# t'w't'w' = var{t'w'}(x,y,z,t) = (t'w' - <t'w'>_xyzt)**2
-dd["twtw_var"] = (dd.tw_cov_tot - stat.tw_cov_tot) * (dd.tw_cov_tot - stat.tw_cov_tot)
-# UNROTATED u'u'u'u' = var{u'u'}(x,y,z,t) = (u'u' - <u'u'>_xyzt)**2
-dd["uuuu_var"] = (dd.uu_var - stat.u_var) * (dd.uu_var - stat.u_var)
-# ROTATED u'u'u'u' = var{u'u'}(x,y,z,t) = (u'u' - <u'u'>_xyzt)**2
-dd["uuuu_var_rot"] = (dd.uu_var_rot - stat.u_var_rot) * (dd.uu_var_rot - stat.u_var_rot)
-# UNROTATED v'v'v'v' = var{v'v'}(x,y,z,t) = (v'v' - <v'v'>_xyzt)**2
-dd["vvvv_var"] = (dd.vv_var - stat.v_var) * (dd.vv_var - stat.v_var)
-# ROTATED v'v'v'v' = var{v'v'}(x,y,z,t) = (v'v' - <v'v'>_xyzt)**2
-dd["vvvv_var_rot"] = (dd.vv_var_rot - stat.v_var_rot) * (dd.vv_var_rot - stat.v_var_rot)
-# w'w'w'w' = var{w'w'}(x,y,z,t) = (w'w' - <w'w'>_xyzt)**2
-dd["wwww_var"] = (dd.ww_var - stat.w_var) * (dd.ww_var - stat.w_var)
-# t't't't' = var{t't'}(x,y,z,t) = (t't' - <t't'>_xyzt)**2
-dd["tttt_var"] = (dd.tt_var - stat.theta_var) * (dd.tt_var - stat.theta_var)
-
-#
-# Calculate autocorrelations for _everything_
-#
-print("Begin calculating autocorrelations...")
-# initialize empty xarray dataset for each autocorrelation and loop
-R = xr.Dataset(data_vars=None,
-               coords=dict(x=dd.x,
-                           z=dd.z)
-              )
-# define list of parameters for looping
-parameters = ["u", "u_rot", "v", "v_rot", "w", "theta",
-              "uw_cov_tot", "vw_cov_tot", "tw_cov_tot", "uu_var", "uu_var_rot", 
-              "vv_var", "vv_var_rot", "ww_var", "tt_var",
-              "uwuw_var", "vwvw_var", "uuuu_var", "uuuu_var_rot", 
-              "vvvv_var", "vvvv_var_rot", "wwww_var", "tttt_var"]
-# for p in parameters:
-#     R[p] = autocorrelation(dd[p])
-
+    #
+    # Perform relaxed filter method for _everything_
+    #
+    print("Begin RFM calculations...")
+    # initialize empty xarray dataset for each RFM and loop
+    RFM = xr.Dataset(data_vars=None,
+                     coords=dict(delta_x=delta_x,
+                                 z=d.z),
+                     attrs=dd.attrs
+                    )
+    # define list of parameters for looping
+    param_RFM = ["u", "u_rot", "v", "v_rot", "theta", 
+                 "uw_cov_tot", "vw_cov_tot", "tw_cov_tot", "uu_var", "uu_var_rot",
+                 "vv_var", "vv_var_rot", "ww_var", "tt_var"]
+    # loop
+    for p in param_RFM:
+        RFM[p] = relaxed_filter(dd[p], delta_x, dd.Lx)
+        
+    #
+    # Save all of these Datasets which took forever to calculate
+    #
+    # 4th order variances, var4
+    fsave_var4 = f"{fdir}variances_4_order.nc"
+    print(f"Saving file: {fsave_var4}")
+    with ProgressBar():
+        var4.to_netcdf(fsave_var4, mode="w")
+    # autocorrelations, R
+    fsave_R = f"{fdir}autocorr.nc"
+    print(f"Saving file: {fsave_R}")
+    with ProgressBar():
+        R.to_netcdf(fsave_R, mode="w")
+    # relaxed filter output, RFM
+    fsave_RFM = f"{fdir}RFM.nc"
+    print(f"Saving file: {fsave_RFM}")
+    with ProgressBar():
+        RFM.to_netcdf(fsave_RFM, mode="w")
+        
+    print("RFMnc.py:main() finished!")
+    return
+        
+# --------------------------------
+# Run script
+# --------------------------------
+if __name__ == "__main__":
+    dt0 = datetime.utcnow()
+    main()
+    dt1 = datetime.utcnow()
+    print(f"Total run time for RFMnc.py: {(dt1-dt0).total_seconds()/60.:5.2f} min")
