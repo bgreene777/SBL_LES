5a6609c1c018ae199c000afae5fcba23f3b8f398
bgreene <bgreene777@gmail.com>
2022-04-19 16:45:44 +0000

    add midpointnormalize class for plotting

--- python/LESnc.py
+++ python/LESnc.py
@@ -13,6 +13,27 @@ import yaml
 import numpy as np
 import xarray as xr
 from dask.diagnostics import ProgressBar
+from matplotlib.colors import Normalize
+# --------------------------------
+# Define plotting class for custom diverging colorbars
+# --------------------------------
+class MidPointNormalize(Normalize):
+    """Defines the midpoint of diverging colormap.
+    Usage: Allows one to adjust the colorbar, e.g. 
+    using contouf to plot data in the range [-3,6] with
+    a diverging colormap so that zero values are still white.
+    Example usage:
+        norm=MidPointNormalize(midpoint=0.0)
+        f=plt.contourf(X,Y,dat,norm=norm,cmap=colormap)
+     """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
 # --------------------------------
 # Define functions
 # --------------------------------
