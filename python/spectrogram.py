3dbbb82fc0db1f1b4ec6c93e74e8163d6f99d413
bgreene <bgreene777@gmail.com>
2022-04-20 19:24:44 +0000

    calculate additional AM coefficients and plot altogether

--- python/spectrogram.py
+++ python/spectrogram.py
@@ -115,8 +115,6 @@ def plot_spectrogram(dnc, figdir):
     Output: saved figures in figdir
     """
     # configure plots
-    rc('font',weight='normal',size=20,family='serif',serif='Times New Roman')
-    rc('text',usetex='True')
     nlevel = 36
     cmap = seaborn.color_palette("cubehelix", as_cmap=True)
     cmap_r = seaborn.color_palette("cubehelix_r", as_cmap=True)
@@ -319,36 +317,29 @@ def amp_mod(dnc, figdir):
     # rotate instantaneous u and v
     d["u_rot"] = d.u*np.cos(angle) + d.v*np.sin(angle)
     d["v_rot"] =-d.u*np.sin(angle) + d.v*np.cos(angle)
-    # calculate covars
-    # u'w'
-    d["uw_cov_res"] = xr.cov(d.u, d.w, dim=("t"))
-    d["uw_cov_tot"] = d.uw_cov_res + d.txz_mean
-    # v'w'
-    d["vw_cov_res"] = xr.cov(d.v, d.w, dim=("t"))
-    d["vw_cov_tot"] = d.vw_cov_res + d.tyz_mean
-    # theta'w'
-    d["tw_cov_res"] = xr.cov(d.theta, d.w, dim=("t"))
-    d["tw_cov_tot"] = d.tw_cov_res + d.q3_mean
-    # calculate vars
-    for s in ["u", "v", "w", "theta"]:
-        d[f"{s}_var"] = d[s].var("t")
-    # recalculate u_var_rot, v_var_rot
-    d["u_var_rot"] = d.u_rot.var("t")
-    d["v_var_rot"] = d.v_rot.var("t")
-    # calculate ustar, L, and z/L
-    d["ustar"] = (d.uw_cov_tot**2. + d.vw_cov_tot**2.) ** 0.25
-    d["L"] = (-1. * (d.ustar**3.) * d.theta_mean) / (0.4 * 9.81 * d.tw_cov_tot)
-    d["zL"] = d.z/d.L
-
+    # calculate "inst" covars
+    d["uw"] = (d.u - d.u_mean) * (d.w - d.w_mean) + d.txz
+    d["vw"] = (d.v - d.v_mean) * (d.w - d.w_mean) + d.tyz
+    d["tw"] = (d.theta - d.theta_mean) * (d.w - d.w_mean) + d.q3
+    # calculate "inst" vars
+    d["uu"] = (d.u - d.u_mean) * (d.u - d.u_mean)
+    d["vv"] = (d.v - d.v_mean) * (d.v - d.v_mean)
+    d["ww"] = (d.w - d.w_mean) * (d.w - d.w_mean)
+    d["tt"] = (d.theta - d.theta_mean) * (d.theta - d.theta_mean)
     # filtering ------------------------------------------------
+    print("Begin calculating amplitude modulation coefficients")
     # lengthscale of filter to separate large and small scales
     # for now will hardcode value for sim A
     delta = 60. # m
     # cutoff frequency from Taylor hypothesis - use same for all heights
     f_c = 1./(delta/d.u_mean_rot)
-    # calculate FFT of u, theta
+    # calculate FFT of u, v, w, theta, uw, tw
     f_u = xrft.fft(d.u_rot, dim="t", true_phase=True, true_amplitude=True)
+    f_v = xrft.fft(d.v_rot, dim="t", true_phase=True, true_amplitude=True)
+    f_w = xrft.fft(d.w, dim="t", true_phase=True, true_amplitude=True)
     f_t = xrft.fft(d.theta, dim="t", true_phase=True, true_amplitude=True)
+    f_uw = xrft.fft(d.uw, dim="t", true_phase=True, true_amplitude=True)
+    f_tw = xrft.fft(d.tw, dim="t", true_phase=True, true_amplitude=True)
     # loop over heights and lowpass filter
     for jz in range(d.nz):
         # can do this all in one line
@@ -356,34 +347,73 @@ def amp_mod(dnc, figdir):
         # only keep -f_c < freq_t < f_c
         f_u[:,jz] = f_u[:,jz].where((f_u.freq_t < f_c.isel(z=jz)) &\
                                     (f_u.freq_t > -f_c.isel(z=jz)), other=0.)
+        f_v[:,jz] = f_v[:,jz].where((f_v.freq_t < f_c.isel(z=jz)) &\
+                                    (f_v.freq_t > -f_c.isel(z=jz)), other=0.)
+        f_w[:,jz] = f_w[:,jz].where((f_w.freq_t < f_c.isel(z=jz)) &\
+                                    (f_w.freq_t > -f_c.isel(z=jz)), other=0.)                                    
         f_t[:,jz] = f_t[:,jz].where((f_t.freq_t < f_c.isel(z=jz)) &\
                                     (f_t.freq_t > -f_c.isel(z=jz)), other=0.)
+        f_uw[:,jz] = f_uw[:,jz].where((f_uw.freq_t < f_c.isel(z=jz)) &\
+                                      (f_uw.freq_t > -f_c.isel(z=jz)), other=0.)   
+        f_tw[:,jz] = f_tw[:,jz].where((f_tw.freq_t < f_c.isel(z=jz)) &\
+                                      (f_tw.freq_t > -f_c.isel(z=jz)), other=0.)                                                                          
 
     # Inverse FFT to get large-scale component of signal
     u_l = xrft.ifft(f_u, dim="freq_t", true_phase=True, true_amplitude=True,
                     lag=f_u.freq_t.direct_lag).real
+    v_l = xrft.ifft(f_v, dim="freq_t", true_phase=True, true_amplitude=True,
+                    lag=f_v.freq_t.direct_lag).real
+    w_l = xrft.ifft(f_w, dim="freq_t", true_phase=True, true_amplitude=True,
+                    lag=f_w.freq_t.direct_lag).real
     t_l = xrft.ifft(f_t, dim="freq_t", true_phase=True, true_amplitude=True,
                     lag=f_t.freq_t.direct_lag).real
+    uw_l = xrft.ifft(f_uw, dim="freq_t", true_phase=True, true_amplitude=True,
+                     lag=f_uw.freq_t.direct_lag).real
+    tw_l = xrft.ifft(f_tw, dim="freq_t", true_phase=True, true_amplitude=True,
+                     lag=f_tw.freq_t.direct_lag).real
     # reset time coordinate
     u_l["t"] = d.t
+    v_l["t"] = d.t
+    w_l["t"] = d.t
     t_l["t"] = d.t
+    uw_l["t"] = d.t
+    tw_l["t"] = d.t
 
     # calculate small-scale component by subtracting large-scale from full
     u_s = d.u_rot - u_l
+    v_s = d.v_rot - v_l
+    w_s = d.w - w_l
     t_s = d.theta - t_l
+    uw_s = d.uw - uw_l
+    tw_s = d.tw - tw_l
 
     # envelope of small-scale signal from Hilbert transform
     E_u = xr.DataArray(data=np.abs(hilbert(u_s, axis=0)),
                        coords=dict(t=u_s.t,
                                    z=u_s.z)    )
+    E_v = xr.DataArray(data=np.abs(hilbert(v_s, axis=0)),
+                       coords=dict(t=v_s.t,
+                                   z=v_s.z)    )
+    E_w = xr.DataArray(data=np.abs(hilbert(w_s, axis=0)),
+                       coords=dict(t=w_s.t,
+                                   z=w_s.z)    )                                   
     E_t = xr.DataArray(data=np.abs(hilbert(t_s, axis=0)),
                        coords=dict(t=t_s.t,
                                    z=t_s.z)    )
-
+    E_uw = xr.DataArray(data=np.abs(hilbert(uw_s, axis=0)),
+                        coords=dict(t=uw_s.t,
+                                    z=uw_s.z)    )                                   
+    E_tw = xr.DataArray(data=np.abs(hilbert(tw_s, axis=0)),
+                        coords=dict(t=tw_s.t,
+                                    z=tw_s.z)    ) 
     # lowpass filter the small-scale envelope
     # fft the envelopes
     f_Eu = xrft.fft(E_u, dim="t", true_phase=True, true_amplitude=True)
+    f_Ev = xrft.fft(E_v, dim="t", true_phase=True, true_amplitude=True)
+    f_Ew = xrft.fft(E_w, dim="t", true_phase=True, true_amplitude=True)
     f_Et = xrft.fft(E_t, dim="t", true_phase=True, true_amplitude=True)
+    f_Euw = xrft.fft(E_uw, dim="t", true_phase=True, true_amplitude=True)
+    f_Etw = xrft.fft(E_tw, dim="t", true_phase=True, true_amplitude=True)
     # loop over heights and lowpass filter - copied from above
     for jz in range(d.nz):
         # can do this all in one line
@@ -391,16 +421,36 @@ def amp_mod(dnc, figdir):
         # only keep -f_c < freq_t < f_c
         f_Eu[:,jz] = f_Eu[:,jz].where((f_Eu.freq_t < f_c.isel(z=jz)) &\
                                       (f_Eu.freq_t > -f_c.isel(z=jz)), other=0.)
+        f_Ev[:,jz] = f_Ev[:,jz].where((f_Ev.freq_t < f_c.isel(z=jz)) &\
+                                      (f_Ev.freq_t > -f_c.isel(z=jz)), other=0.)    
+        f_Ew[:,jz] = f_Ew[:,jz].where((f_Ew.freq_t < f_c.isel(z=jz)) &\
+                                      (f_Ew.freq_t > -f_c.isel(z=jz)), other=0.)                                                                        
         f_Et[:,jz] = f_Et[:,jz].where((f_Et.freq_t < f_c.isel(z=jz)) &\
                                       (f_Et.freq_t > -f_c.isel(z=jz)), other=0.)
+        f_Euw[:,jz] = f_Euw[:,jz].where((f_Euw.freq_t < f_c.isel(z=jz)) &\
+                                        (f_Euw.freq_t > -f_c.isel(z=jz)), other=0.) 
+        f_Etw[:,jz] = f_Etw[:,jz].where((f_Etw.freq_t < f_c.isel(z=jz)) &\
+                                        (f_Etw.freq_t > -f_c.isel(z=jz)), other=0.)                                                                              
     # inverse fft the filtered envelopes
     E_u_f = xrft.ifft(f_Eu, dim="freq_t", true_phase=True, true_amplitude=True,
                       lag=f_Eu.freq_t.direct_lag).real
+    E_v_f = xrft.ifft(f_Ev, dim="freq_t", true_phase=True, true_amplitude=True,
+                      lag=f_Ev.freq_t.direct_lag).real   
+    E_w_f = xrft.ifft(f_Ew, dim="freq_t", true_phase=True, true_amplitude=True,
+                      lag=f_Ew.freq_t.direct_lag).real                                         
     E_t_f = xrft.ifft(f_Et, dim="freq_t", true_phase=True, true_amplitude=True,
                       lag=f_Et.freq_t.direct_lag).real
+    E_uw_f = xrft.ifft(f_Euw, dim="freq_t", true_phase=True, true_amplitude=True,
+                       lag=f_Euw.freq_t.direct_lag).real          
+    E_tw_f = xrft.ifft(f_Etw, dim="freq_t", true_phase=True, true_amplitude=True,
+                       lag=f_Etw.freq_t.direct_lag).real                                             
     # reset time coordinate
     E_u_f["t"] = d.t
+    E_v_f["t"] = d.t
+    E_w_f["t"] = d.t
     E_t_f["t"] = d.t
+    E_uw_f["t"] = d.t
+    E_tw_f["t"] = d.t
 
     # AM coefficients ------------------------------------------------
     # new dataset to hold corr coeff
@@ -410,35 +460,74 @@ def amp_mod(dnc, figdir):
                   )
     # correlation between large scale u and filtered envelope of small-scale variable
     R["ul_Eu"] = xr.corr(u_l, E_u_f, dim="t")
+    R["ul_Ew"] = xr.corr(u_l, E_w_f, dim="t")
     R["ul_Et"] = xr.corr(u_l, E_t_f, dim="t")
+    R["ul_Euw"] = xr.corr(u_l, E_uw_f, dim="t")
+    R["ul_Etw"] = xr.corr(u_l, E_tw_f, dim="t")
+    # correlation between large scale w and filtered envelope of small-scale variable
+    R["wl_Eu"] = xr.corr(w_l, E_u_f, dim="t")
+    R["wl_Ew"] = xr.corr(w_l, E_w_f, dim="t")
+    R["wl_Et"] = xr.corr(w_l, E_t_f, dim="t")
+    R["wl_Euw"] = xr.corr(w_l, E_uw_f, dim="t")
+    R["wl_Etw"] = xr.corr(w_l, E_tw_f, dim="t")    
     # correlation between large scale u and filtered envelope of small-scale variable
     R["tl_Et"] = xr.corr(t_l, E_t_f, dim="t")
-    # TODO: add more variables
+    
 
     # Plot ------------------------------------------------
-    # figure 1 - two panels
-    # (a) modulation of small-scale u by large-scale u: R_ul_Eu
-    # (b) modulation of small-scale theta by large-scale u: R_tl_Et
-    fig1, ax1 = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,
-                             figsize=(9.8, 5))
-    # (a) R_ul_Eu vs z/h
-    ax1[0].plot(R.z/stat.he, R.ul_Eu, "-k")
-    # (b) R_ul_Et vs z/h
-    ax1[1].plot(R.z/stat.he, R.ul_Et, "-k")
+    print("Begin plotting figure 1")
+    # figure 1 - ten panels - modulation by u_l and w_l
+    # col 1 - modulation by u_l
+    # col 2 - modulation by w_l
+    # row 1 - modulation of small-scale u by large-scale u&w
+    # row 2 - modulation of small-scale w by large-scale u&w
+    # row 3 - modulation of small-scale theta by large-scale u&w
+    # row 4 - modulation of small-scale uw by large-scale u&w
+    # row 5 - modulation of small-scale tw by large-scale u&w
+    fig1, ax1 = plt.subplots(nrows=5, ncols=2, sharex=True, sharey=True,
+                             figsize=(9.8, 16), constrained_layout=True)
+    # (a) R_ul_Eu
+    ax1[0,0].plot(R.z/stat.he, R.ul_Eu, "-k")
+    ax1[0,0].set_ylabel("$R_{u_l,u_s}$")
+    # (b) R_wl_Eu
+    ax1[0,1].plot(R.z/stat.he, R.wl_Eu, "-k")
+    ax1[0,1].set_ylabel("$R_{w_l,u_s}$")
+    # (c) R_ul_Ew
+    ax1[1,0].plot(R.z/stat.he, R.ul_Ew, "-k")
+    ax1[1,0].set_ylabel("$R_{u_l,w_s}$")
+    # (d) R_wl_Ew
+    ax1[1,1].plot(R.z/stat.he, R.wl_Ew, "-k")
+    ax1[1,1].set_ylabel("$R_{w_l,w_s}$")
+    # (e) R_ul_Et
+    ax1[2,0].plot(R.z/stat.he, R.ul_Et, "-k")
+    ax1[2,0].set_ylabel("$R_{u_l,\\theta_s}$")
+    # (f) R_wl_Et
+    ax1[2,1].plot(R.z/stat.he, R.wl_Et, "-k")
+    ax1[2,1].set_ylabel("$R_{w_l,\\theta_s}$")
+    # (g) R_ul_Euw
+    ax1[3,0].plot(R.z/stat.he, R.ul_Euw, "-k")
+    ax1[3,0].set_ylabel("$R_{u_l,(uw)_s}$")
+    # (h) R_wl_Euw
+    ax1[3,1].plot(R.z/stat.he, R.wl_Euw, "-k")
+    ax1[3,1].set_ylabel("$R_{w_l,(uw)_s}$")
+    # (i) R_ul_Etw
+    ax1[4,0].plot(R.z/stat.he, R.ul_Etw, "-k")
+    ax1[4,0].set_ylabel("$R_{u_l,(\\theta w)_s}$")
+    # (j) R_wl_Etw
+    ax1[4,1].plot(R.z/stat.he, R.wl_Etw, "-k")
+    ax1[4,1].set_ylabel("$R_{w_l,(\\theta w)_s}$")
+
     # clean up
-    ax1[0].set_xlabel("$z/h$")
-    ax1[0].set_ylabel("$R_{u_l,u_s}$")
-    ax1[0].set_ylim([-0.5, 0.5])
-    ax1[0].set_xscale("log")
-    ax1[1].set_xlabel("$z/h$")
-    ax1[1].set_ylabel("$R_{u_l,\\theta_s}$")
+    ax1[4,0].set_xlabel("$z/h$")
+    ax1[0,0].set_ylim([-0.5, 0.5])
+    ax1[0,0].set_xscale("log")
+    ax1[4,1].set_xlabel("$z/h$")
     # plot ref lines
     for iax in ax1.flatten():
         iax.axhline(0, c="k", ls="-", alpha=0.5)
         iax.axvline(delta/stat.he, c="k", ls="-", alpha=0.5)
 
     # save
-    fig1.tight_layout()
     fsave1 = f"{figdir}{R.stability}_amp_mod.png"
     print(f"Saving figure {fsave1}")
     fig1.savefig(fsave1, dpi=300)
@@ -470,10 +559,14 @@ def amp_mod(dnc, figdir):
 # main
 # --------------------------------
 if __name__ == "__main__":
+    # configure plotting parameters
+    rc('font',weight='normal',size=20,family='serif',serif='Times New Roman')
+    rc('text',usetex='True')
+    # figure save directories
     figdir = "/home/bgreene/SBL_LES/figures/spectrogram/"
     figdir_AM = "/home/bgreene/SBL_LES/figures/amp_mod/"
     # loop sims A--F
-    for sim in list("A"):
+    for sim in list("B"):
         print(f"---Begin Sim {sim}---")
         ncdir = f"/home/bgreene/simulations/{sim}_192_interp/output/netcdf/"
         # calc_spectra(ncdir)
